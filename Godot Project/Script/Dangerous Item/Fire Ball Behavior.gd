8e2abc147206a934d7bea1af2e968f4aa5a28514
Neo <neo.cyc@gmail.com>
2023-07-08 16:03:50 -0700

    Fireball Spawn Successful

--- Godot Project/Script/Dangerous Item/Fire Ball Behavior.gd
+++ Godot Project/Script/Dangerous Item/Fire Ball Behavior.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 # Fire Ball move in a Diaganal
-var direction = Vector2(-0.75,1)
+var direction = Vector2(-0.9,0.8)
 var speed
 var bound
 
@@ -10,14 +10,17 @@ var anim_node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	bound = Vector2(0, get_viewport_rect().size.y)
-	speed = get_node("/root/Main").fireball_speed
 	anim_node = get_node("Firebal_2D/Fireball Anim")
 	anim_node.play("Spin")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	# Constant Update with Main's Speed
+	speed = get_node("/root/Main").fireball_speed
+	
 	# Fire Ball move downward from their Initiated Pos
-	if(global_position.x <= bound.x or global_position.y >= bound.y):
+	if(global_position.x <= bound.x or global_position.y >= bound.y-100):
 		print("destroy")
 		_free_Object()
 	global_position.x += direction.x * speed * delta
--- Godot Project/Script/Main Game/game_manager.gd
+++ Godot Project/Script/Main Game/game_manager.gd
@@ -1,7 +1,11 @@
 extends Node
 
 var can_spawn = true
+# Fireball Related Variables
 var fireball_speed = 200.0
+var fireball_spawn_rate = 6.0 # seconds
+
+# Platform Related Variables
 var platform_speed = 250
 
 func can_spawn_state(state : bool):
