0d35e54bd9c6dd36c9d6dc12d3fc7783dce1a921
Neo <neo.cyc@gmail.com>
2023-07-09 00:51:39 -0700

    Revert "Revert "Merge pull request #6 from Ne0Zhang/player-platform-implementation""
    This reverts commit ac1113f04e1e891d4d0a23f5906be8383fe98ab5.

--- Godot Project/Script/Cloud Platform/Cloud_Platform_Behavior.gd
+++ Godot Project/Script/Cloud Platform/Cloud_Platform_Behavior.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+signal slide
+
 # Lower cap for the rotation
 @export var min_rotate = -60.0
 # Upper cap for the rotation
@@ -34,7 +36,6 @@ func _input(event):
 			assoc_button._reset_sprite()
 			parent.can_move = true
 			$CollisionShape2D.set_deferred("disabled", false)
-			get_node("JumpBox/CollisionShape2D").set_deferred("disabled", false)
 
 	if event is InputEventMouseButton and selected:
 		if Input.is_action_just_pressed("Rotate_Right"):
@@ -63,4 +64,3 @@ func _ready():
 	assoc_button = get_node("/root/Main/Button Collection/Cloud Spawn Button/Cloud Button")
 	parent = get_owner()
 	$CollisionShape2D.set_deferred("disabled", true)
-	get_node("JumpBox/CollisionShape2D").set_deferred("disabled", true)
