776066e0464c53e3e6dbbb8e530e181ba15459e3
Ashley <60424761+bluemew0@users.noreply.github.com>
2023-07-09 02:03:43 -0700

    volume tick fixed

--- Godot Project/Script/tutorial.gd
+++ Godot Project/Script/tutorial.gd
@@ -1,20 +1,6 @@
 extends Control
 @onready var bean = $bean
-@onready var pause_settings = $"Pause Settings"
 
 func _ready():
-	pause_settings.destination = "tutorial"
 	bean.play("run")
-	unpaused()
 
-func paused():
-	get_tree().paused = true
-	pause_settings.show()
-	
-func unpaused():
-	pause_settings.hide()
-	get_tree().paused = false
-	
-func _on_back_pressed():
-	$".".hide()
-	
