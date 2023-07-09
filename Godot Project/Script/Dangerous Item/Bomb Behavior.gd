732e6347a7490a3e4aee5d2141cae99ac7354c33
Neo <neo.cyc@gmail.com>
2023-07-08 18:05:47 -0700

    Main Global

--- Godot Project/Script/Dangerous Item/Bomb Behavior.gd
+++ Godot Project/Script/Dangerous Item/Bomb Behavior.gd
@@ -3,9 +3,11 @@ extends Node2D
 # Fire Ball move in a Diaganal
 var direction = Vector2(-1,0)
 var bound
+var speed
 
 var audio_node
 var anim_node
+var played = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,23 +17,25 @@ func _ready():
 	audio_node = get_node("Explode_Audio")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	# Constant Update with Main's Speed
-	var speed = get_node("/root/Main").bomb_speed
-	
+func _process(delta):	
 	# Fire Ball move downward from their Initiated Pos
 	if(global_position.x <= bound.x - 100):
 		_free_object()
-	global_position.x += direction.x * speed * delta
+	global_position.x += direction.x * get_node("/root/Main").platform_speed * delta
 
 func _free_object():
+	speed = 0
 	anim_node.stop()
 	print("Bomb Destroy")
 	queue_free()
 
 # If player touches bomb, bomb explode and dies
 func _explode():
+	speed = 0
+	played = false
 	anim_node.play("Explode")
 	audio_node.play()
 	await anim_node.animation_finished
+	anim_node.visible = false
+	await audio_node.finished
 	queue_free()
--- Godot Project/Script/Dangerous Item/Fire Ball Behavior.gd
+++ Godot Project/Script/Dangerous Item/Fire Ball Behavior.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 # Fire Ball move in a Diaganal
 var direction = Vector2(-0.9,0.8)
-var speed
 var bound
 
 var anim_node
@@ -16,15 +15,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	# Constant Update with Main's Speed
-	speed = get_node("/root/Main").fireball_speed
-	
 	# Fire Ball move downward from their Initiated Pos
 	if(global_position.x <= bound.x or global_position.y >= bound.y-100):
 		print("destroy")
 		_free_Object()
-	global_position.x += direction.x * speed * delta
-	global_position.y += direction.y * speed * delta
+	global_position.x += direction.x * get_node("/root/Main").platform_speed * delta
+	global_position.y += direction.y * get_node("/root/Main").fireball_speed * delta
 
 func _free_Object():
 	anim_node.stop()
